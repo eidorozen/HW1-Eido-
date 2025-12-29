d5ee09d60be11548a43cf758ecaa93f9790be0b8
eidorozen <eidorozen.er@gmail.com>
2025-12-29 17:51:35 +0200

    Update StandardAccount[1].java

--- StandardAccount[1].java
+++ StandardAccount[1].java
@@ -1,7 +1,54 @@
-public class StandardAccount {
+public class StandardAccount implements IAccount {
 
-    public StandardAccount(int accountNumber,double creditLimit) {
+    private int accountNumber;
+    private double balance;
+    private double creditLimit;
 
+    public StandardAccount(int accountNumber, double creditLimit) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+
+        if (creditLimit < 0) {
+            this.creditLimit = creditLimit;
+        } else {
+            this.creditLimit = 0;
+        }
     }
 
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public double withdraw(double amount) {
+        if (amount <= 0) {
+            return 0;
+        }
+
+        double allowedAmount = amount;
+
+        if (balance - allowedAmount < creditLimit) {
+            allowedAmount = balance - creditLimit;
+        }
+
+        if (allowedAmount < 0) {
+            return 0;
+        }
+
+        balance -= allowedAmount;
+        return allowedAmount;
+    }
+
+    @Override
+    public double getCurrentBalance() {
+        return balance;
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
 }
