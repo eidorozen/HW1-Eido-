a40a7a6d3b19f47409c051e2a6aff4171f1007fe
eidorozen <eidorozen.er@gmail.com>
2025-12-29 17:49:38 +0200

    Update BasicAccount[1].java

--- BasicAccount[1].java
+++ BasicAccount[1].java
@@ -1,7 +1,49 @@
-public class BasicAccount {
+public class BasicAccount implements IAccount {
 
-    public BasicAccount (int accountNumber,double withdrawLimit) {
+    private int accountNumber;
+    private double balance;
+    private double withdrawLimit;
 
+    public BasicAccount(int accountNumber, double withdrawLimit) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+        this.withdrawLimit = withdrawLimit;
     }
 
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public double withdraw(double amount) {
+        if (amount <= 0) {
+            return 0;
+        }
+
+        double allowedAmount = amount;
+
+        if (allowedAmount > withdrawLimit) {
+            allowedAmount = withdrawLimit;
+        }
+
+        if (balance - allowedAmount < 0) {
+            allowedAmount = balance;
+        }
+
+        balance -= allowedAmount;
+        return allowedAmount;
+    }
+
+    @Override
+    public double getCurrentBalance() {
+        return balance;
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
 }
